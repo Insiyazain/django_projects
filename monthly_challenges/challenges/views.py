9f03cccbe9f637eeaaf984863b5d462245f52c46
insiyazain <insiyazain123@gmail.com>
2024-09-25 15:18:04 +0500

    Basic view file changes

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+
+def monthly_challenge(request, month):
+    challenge_text=None
+    if month=="january":
+        challenge_text="Eat no meat for the entire month!"
+    elif month=="february":
+        challenge_text="walk for 20 min a day"
+    elif month=="March":
+        challenge_text="Learn django 20 min a day"
+    else:
+        return HttpResponseNotFound("This month is not found")
+    return HttpResponse(challenge_text)
+
+
+    return HttpResponse("Walk for atleast 20 minutes every day")
