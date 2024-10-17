f61bec5b07cea5fb86aae3c5dabdea69d1a04c55
insiyazain <insiyazain123@gmail.com>
2024-10-17 08:19:51 +0500

    html and css coding

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -13,6 +13,17 @@ monthly_challenges={
     "june":"Learn django for atleast 20 minutes every day"
 }
 
+def list_of_challenges(request):
+    months=list(monthly_challenges.keys())
+    response_form=""
+
+    for month in months:
+        capitilized_month=month.capitalize()
+        response_html=reverse("monthly-challenge",args=[month])
+        response_form+=f"<li><a href=\"{response_html}\">{capitilized_month}</a></li>"
+
+    return HttpResponse(response_form)
+
 def monthly_challenges_by_number(request, month):
     months=list(monthly_challenges.keys())
 
@@ -26,6 +37,7 @@ def monthly_challenges_by_number(request, month):
 def monthly_challenge(request,month):
    try:
        challenge_text=monthly_challenges[month]
-       return HttpResponse(challenge_text)
+       response_data=f"<h1>{challenge_text}</h1>"
+       return HttpResponse(response_data)
    except:
-       return HttpResponseNotFound("This month is not supported")
+       return HttpResponseNotFound("<h2>This month is not supported<h2>")
