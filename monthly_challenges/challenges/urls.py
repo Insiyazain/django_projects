badf2569d28d5e0489f6b90c70c72e36f74986e8
insiyazain <insiyazain123@gmail.com>
2024-10-21 08:28:03 +0500

    include use

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-   path("",views.list_of_challenges),
+   path("",views.list_of_challenges,name="index"),
    path("<int:month>",views.monthly_challenges_by_number), 
    path("<str:month>", views.monthly_challenge, name="monthly-challenge")
    
